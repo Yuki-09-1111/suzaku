2f43bbd69bca67030133d676e4a3cb375b764fd6
masashi.sato <gold.pocky.y.11.11@i.softbank.jp>
2019-11-16 11:24:22 +0000

    a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
-  protect_from_forgery with: :exception
   http_basic_authenticate_with :name => ENV['BASIC_USERNAME'], :password => ENV['BASIC_PASSWORD'] if Rails.env == "production"
 
   include SessionsHelper
